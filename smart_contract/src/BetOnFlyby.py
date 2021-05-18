f2f24e63cc9ed2b016e2525d82aeb45f1cc92968
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2021-05-18 19:49:38 -0300

    betting logic

--- smart_contract/src/BetOnFlyby.py
+++ smart_contract/src/BetOnFlyby.py
@@ -1,10 +1,13 @@
-from typing import Any
+from typing import Any, List, cast
 
 from boa3.builtin import CreateNewEvent, public
-from boa3.builtin.interop.contract import destroy_contract, update_contract
-from boa3.builtin.interop.runtime import calling_script_hash, check_witness
-from boa3.builtin.interop.storage import get, put
-from boa3.builtin.type import UInt160
+from boa3.builtin.contract import abort
+from boa3.builtin.interop.binary import deserialize, serialize
+from boa3.builtin.interop.blockchain import Transaction
+from boa3.builtin.interop.contract import GAS, call_contract, destroy_contract, update_contract
+from boa3.builtin.interop.runtime import calling_script_hash, check_witness, executing_script_hash, script_container
+from boa3.builtin.interop.storage import find, get, put
+from boa3.builtin.type import UInt160, UInt256
 
 # -------------------------------------------
 # EVENTS
@@ -18,16 +21,203 @@ on_change_image = CreateNewEvent([('sender', UInt160)],
 # -------------------------------------------
 
 OWNER_KEY = b'OWNER'
-
+BET_OWNER_KEY = b'bet_owner_'
+BET_TOTAL_STAKE_KEY = b'bet_total_stake_'
+BET_OPTIONS_KEY = b'bet_options_'
+BET_DESCRIPTION_KEY = b'bet_description_'
+BET_RESULT_KEY = b'bet_result_'
+BET_VOTE_KEY = b'bet_vote_'
 
 # -------------------------------------------
 # CONTRACT LOGIC
 # -------------------------------------------
 
+PRICE_IN_GAS = 1 * 10 ** 8  # bet cost is 1 GAS
+
+
+@public
+def get_bet(bet_id: UInt256) -> list:
+    creator = get(BET_OWNER_KEY + bet_id)
+
+    if len(creator) == 0:
+        raise Exception("Bet doesn't exist.")
+
+    description = get(BET_DESCRIPTION_KEY + bet_id).to_str()
+    options: List[str] = deserialize(get(BET_OPTIONS_KEY + bet_id))
+
+    result = None
+    serialized_result = get(BET_RESULT_KEY + bet_id)
+    if len(serialized_result) > 0:
+        result = deserialize(serialized_result)
+
+    return [bet_id,
+            creator,
+            description,
+            options,
+            result
+            ]
+
+
+@public
+def create_bet(creator: UInt160, description: str, options: List[str]) -> UInt256:
+    if not check_witness(creator):
+        raise Exception('No authorization.')
+
+    options: List[str] = remove_duplicates(options)
+    if len(options) < 2:
+        raise Exception('Not enough options to create a bet')
+
+    for option in options:
+        if len(option) == 0:
+            raise Exception('Cannot have an empty option')
+
+    tx: Transaction = script_container
+    bet_id = tx.hash
+
+    put(BET_OWNER_KEY + bet_id, creator)
+    put(BET_TOTAL_STAKE_KEY + bet_id, 0)
+    put(BET_OPTIONS_KEY + bet_id, serialize(options))
+    put(BET_DESCRIPTION_KEY + bet_id, description)
+
+    request_image_change()
+
+    return bet_id
+
+
+def remove_duplicates(list_with_dups: list) -> list:
+    new_list = []
+    for value in list_with_dups:
+        if value not in new_list:
+            new_list.append(value)
+
+    return new_list
+
+
+@public
+def finish_bet(bet_id: UInt256, winner_options: List[str]):
+    creator = get(BET_OWNER_KEY + bet_id)
+
+    if len(creator) == 0:
+        raise Exception("Bet doesn't exist.")
+    if not check_witness(creator):
+        raise Exception('No authorization.')
+    if len(get(BET_RESULT_KEY + bet_id)) > 0:
+        raise Exception("Bet is finished already")
+    if len(winner_options) == 0:
+        raise Exception('At least one winner is required')
+
+    # validate all winner options are valid options
+    winner_options: List[str] = remove_duplicates(winner_options)
+    bet_options: List[str] = deserialize(get(BET_OPTIONS_KEY + bet_id))
+    for option in winner_options:
+        if option not in bet_options:
+            raise Exception('Invalid option for this bet')
+
+    winners: List[UInt160] = []
+
+    # get winner players
+    votes_key_prefix = BET_VOTE_KEY + bet_id
+    vote = find(votes_key_prefix)
+    if vote.next():
+        result_pair = vote.value
+        storage_key = cast(bytes, result_pair[0])
+        account_vote = cast(str, result_pair[1])
+
+        if account_vote in winner_options:
+            # slice is not implemented for bytes on neo3-boa v0.8.1
+            address = storage_key[len(votes_key_prefix):]
+            account = UInt160(address)
+            winners.append(account)
+
+    # distribute the prizes
+    if len(winners) > 0:
+        total_stake = get(BET_TOTAL_STAKE_KEY + bet_id).to_int()
+        prize_per_winner = total_stake // len(winners)
+        executing_contract = executing_script_hash
+
+        for winner in winners:
+            transfer_gas(executing_contract, winner, prize_per_winner)
+
+    # set result
+    put(BET_RESULT_KEY + bet_id, serialize(winner_options))
+
+
+@public
+def cancel_bet(bet_id: UInt256):
+    creator_on_storage = get(BET_OWNER_KEY + bet_id)
+
+    if len(creator_on_storage) == 0:
+        raise Exception("Bet doesn't exist.")
+
+    creator = UInt160(creator_on_storage)
+    if not check_witness(creator):
+        raise Exception('No authorization.')
+    if len(get(BET_RESULT_KEY + bet_id)) > 0:
+        raise Exception("Bet is finished already")
+
+    # 5% fee of total stake for cancelling the bet
+    total_stake = get(BET_TOTAL_STAKE_KEY + bet_id).to_int()
+    executing_contract = executing_script_hash
+    transfer_gas(creator, executing_contract, total_stake * 5 // 100)
+
+    # refund players
+    votes_key_prefix = BET_VOTE_KEY + bet_id
+    vote = find(votes_key_prefix)
+    if vote.next():
+        result_pair: List[bytes] = vote.value
+        storage_key = result_pair[0]
+
+        account = UInt160(storage_key[len(votes_key_prefix):])
+        transfer_gas(executing_contract, account, PRICE_IN_GAS)
+
+    # set result
+    put(BET_RESULT_KEY + bet_id, serialize('Cancelled by owner'))
+
+
+@public
+def bet(player: UInt160, bet_id: UInt256, bet_option: str):
+    if len(get(BET_OWNER_KEY + bet_id)) == 0:
+        raise Exception("Bet doesn't exist.")
+    if not check_witness(player):
+        raise Exception('No authorization.')
+    if len(get(BET_RESULT_KEY + bet_id)) > 0:
+        raise Exception("Bet is finished already")
+
+    player_vote_key = BET_VOTE_KEY + bet_id + player
+    if len(get(player_vote_key)) > 0:
+        raise Exception('Only one bet is allowed per account')
+
+    valid_options: List[str] = deserialize(get(BET_OPTIONS_KEY + bet_id))
+    if bet_option not in valid_options:
+        raise Exception('Invalid option for this bet')
+
+    total_stake = get(BET_TOTAL_STAKE_KEY + bet_id).to_int()
+    total_stake += PRICE_IN_GAS
+
+    transfer_gas(player, executing_script_hash, PRICE_IN_GAS)
+    put(player_vote_key, bet_option)
+    put(BET_TOTAL_STAKE_KEY + bet_id, total_stake)
+
+    request_image_change()
+
+
 @public
 def request_image_change():
     invoker = calling_script_hash
     on_change_image(invoker)
+
+
+def transfer_gas(from_account: UInt160, to_account: UInt160, amount: int):
+    success: bool = call_contract(GAS, 'transfer', [from_account, to_account, amount, None])
+    if not success:
+        raise Exception('GAS transfer was not successful')
+
+
+@public
+def onNEP17Payment(from_address: UInt160, amount: int, data: Any):
+    # accept GAS only
+    if calling_script_hash != GAS:
+        abort()
 
 
 # -------------------------------------------
@@ -45,7 +235,7 @@ def _deploy(data: Any, update: bool):
         # it was deployed already
         return
 
-    put(OWNER_KEY, calling_script_hash)
+    put(OWNER_KEY, "NMmy263woLS5thu238tj2WSzcYQNrP4ZqV".to_script_hash())
 
 
 @public
--- smart_contract/test/testBetOnFlyby.py
+++ smart_contract/test/testBetOnFlyby.py
@@ -1,7 +1,10 @@
 import os.path
 import unittest
+from typing import List
 
 from boa3.boa3 import Boa3
+from boa3.neo.smart_contract.VoidType import VoidType
+from boa3.neo3.vm import VMState
 from boa3_test.tests.test_classes.testengine import TestEngine
 
 
@@ -16,12 +19,465 @@ class TestExample(unittest.TestCase):
         test_engine_installation_folder = cls.dirname   # Change this to your test engine installation folder
         cls.engine = TestEngine(test_engine_installation_folder)
 
-    def test_example(self):
-        path = f'{self.dirname}/src/BetOnFlyby.py'
-        nef_path = path.replace('.py', '.nef')
+        path = f'{cls.dirname}/src/BetOnFlyby.py'
+        cls.nef_path = path.replace('.py', '.nef')
 
-        Boa3.compile_and_save(path, output_path=nef_path)
+        if not os.path.isfile(cls.nef_path):
+            Boa3.compile_and_save(path, output_path=cls.nef_path)
 
+    def test_request_image_change(self):
         self.engine.reset_engine()
-        result = self.engine.run(nef_path, 'request_image_change')
-        self.assertIsNone(result)  # calling_script_hash in the test engine is None
+        on_change_image_event_name = 'ChangeImage'
+
+        result = self.engine.run(self.nef_path, 'request_image_change')
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+        self.assertEqual(VoidType, result)
+
+        event_notifications = self.engine.get_events(event_name=on_change_image_event_name)
+        self.assertEqual(1, len(event_notifications))
+
+    def _create_bet(self, creator_account: bytes, description: str, options: List[str]):
+        self.engine.add_signer_account(creator_account)
+        return self.engine.run(self.nef_path, 'create_bet', creator_account, description, options)
+
+    def test_create_bet_success(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        result = self._create_bet(creator_account, description, options)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+        self.assertIsInstance(result, bytes)
+        self.assertEqual(32, len(result))
+
+    def test_create_bet_fail_check_witness(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = []
+
+        # need signing
+        self.engine.run(self.nef_path, 'create_bet', creator_account, description, options)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('No authorization.'))
+
+    def test_create_bet_fail_not_enough_options(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = []
+
+        self.engine.add_signer_account(creator_account)
+        # need at least two different options
+        self.engine.run(self.nef_path, 'create_bet', creator_account, description, options)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('Not enough options to create a bet'))
+
+        options = ['choice1', 'choice1']  # need at least two different options
+        self.engine.add_signer_account(creator_account)
+        self.engine.run(self.nef_path, 'create_bet', creator_account, description, options)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('Not enough options to create a bet'))
+
+    def test_create_bet_fail_empty_option(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', '', 'choice2']
+
+        self.engine.add_signer_account(creator_account)
+
+        self.engine.run(self.nef_path, 'create_bet', creator_account, description, options)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('Cannot have an empty option'))
+
+    def _bet(self, bet_id: bytes, player: bytes, option: str):
+        price_in_gas = 1 * 10 ** 8  # 1 GAS
+        self.engine.add_gas(player, price_in_gas)
+        self.engine.add_signer_account(player)
+        self.engine.run(self.nef_path, 'bet', player, bet_id, option)
+
+    def test_bet_success(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        player = bytes(range(20))
+        bet_option = 'choice1'
+        price_in_gas = 1 * 10 ** 8  # 1 GAS
+
+        self.engine.add_signer_account(player)
+        self.engine.add_gas(player, price_in_gas)
+        result = self.engine.run(self.nef_path, 'bet', player, bet_id, bet_option)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+        self.assertEqual(VoidType, result)
+
+    def test_bet_fail_doesnt_exist(self):
+        self.engine.reset_engine()
+
+        bet_id = bytes(32)
+        player = bytes(20)
+        bet_option = 'choice1'
+
+        self.engine.run(self.nef_path, 'bet', player, bet_id, bet_option)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith("Bet doesn't exist."))
+
+    def test_bet_fail_check_witness(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        player = bytes(range(20))
+        bet_option = 'choice1'
+
+        self.engine.run(self.nef_path, 'bet', player, bet_id, bet_option)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('No authorization.'))
+
+    def test_bet_fail_finished_already(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        bet_option = 'choice1'
+        self._finish_bet(creator_account, bet_id, [bet_option])
+        player = bytes(range(20))
+
+        self._bet(bet_id, player, bet_option)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('Bet is finished already'))
+
+    def test_bet_fail_voted_already(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        player = bytes(range(20))
+        bet_option = 'choice1'
+
+        self._bet(bet_id, player, bet_option)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+        self._bet(bet_id, player, 'choice2')
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('Only one bet is allowed per account'))
+
+    def test_bet_fail_invalid_option(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        player = bytes(range(20))
+        bet_option = 'choice4'
+
+        self._bet(bet_id, player, bet_option)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('Invalid option for this bet'))
+
+    def test_bet_fail_not_enough_gas(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        player = bytes(range(20))
+        bet_option = 'choice1'
+
+        self.engine.add_signer_account(player)
+        self.engine.run(self.nef_path, 'bet', player, bet_id, bet_option)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('GAS transfer was not successful'))
+
+    def _finish_bet(self, creator_account: bytes, bet_id: bytes, winners: List[str]):
+        self.engine.add_signer_account(creator_account)
+        self.engine.run(self.nef_path, 'finish_bet', bet_id, winners)
+
+    def test_finish_bet_success_one_winner(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        player = bytes(range(20))
+        bet_option = 'choice2'
+        self._bet(bet_id, player, bet_option)
+
+        winner_option = [bet_option]
+        self.engine.add_signer_account(creator_account)
+        self.engine.run(self.nef_path, 'finish_bet', bet_id, winner_option)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+    def test_finish_bet_success_two_winners(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        player = bytes(range(20))
+        bet_option = 'choice2'
+        self._bet(bet_id, player, bet_option)
+
+        winner_option = ['choice1', bet_option]
+        self.engine.add_signer_account(creator_account)
+        self.engine.run(self.nef_path, 'finish_bet', bet_id, winner_option)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+    def test_finish_bet_fail_doesnt_exist(self):
+        self.engine.reset_engine()
+
+        bet_id = bytes(32)
+        winner_option = []
+
+        self.engine.run(self.nef_path, 'finish_bet', bet_id, winner_option)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith("Bet doesn't exist."))
+
+    def test_finish_bet_fail_check_witness(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        winner_option = ['choice1']
+
+        self.engine.run(self.nef_path, 'finish_bet', bet_id, winner_option)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('No authorization.'))
+
+    def test_finish_bet_fail_finished_already(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        winner_option = ['choice1']
+        self._finish_bet(creator_account, bet_id, winner_option)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+        self._finish_bet(creator_account, bet_id, winner_option)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('Bet is finished already'))
+
+    def test_finish_bet_fail_not_enough_winner_options(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        winner_option = []
+
+        self.engine.add_signer_account(creator_account)
+        self.engine.run(self.nef_path, 'finish_bet', bet_id, winner_option)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('At least one winner is required'))
+
+    def test_finish_bet_fail_invalid_winner_option(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        winner_option = ['choice4']
+
+        self.engine.add_signer_account(creator_account)
+        self.engine.run(self.nef_path, 'finish_bet', bet_id, winner_option)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('Invalid option for this bet'))
+
+    def _cancel_bet(self, creator_account: bytes, bet_id: bytes):
+        self.engine.add_signer_account(creator_account)
+        self.engine.run(self.nef_path, 'cancel_bet', bet_id)
+
+    def test_cancel_bet_success(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+
+        self.engine.add_signer_account(creator_account)
+        self.engine.run(self.nef_path, 'cancel_bet', bet_id)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+    def test_cancel_bet_fail_doesnt_exist(self):
+        self.engine.reset_engine()
+
+        bet_id = bytes(32)
+
+        self.engine.run(self.nef_path, 'cancel_bet', bet_id)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith("Bet doesn't exist."))
+
+    def test_cancel_bet_fail_check_witness(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+
+        self.engine.run(self.nef_path, 'cancel_bet', bet_id)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('No authorization.'))
+
+    def test_cancel_bet_fail_finished_already(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        winner_option = ['choice1']
+        self._finish_bet(creator_account, bet_id, winner_option)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+        self.engine.add_signer_account(creator_account)
+        self.engine.run(self.nef_path, 'cancel_bet', bet_id)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('Bet is finished already'))
+
+    def test_cancel_bet_fail_cannot_pay_fee(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        player = bytes(range(20))
+        bet_option = 'choice1'
+
+        self._bet(bet_id, player, bet_option)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+        self.engine.add_signer_account(creator_account)
+        self.engine.run(self.nef_path, 'cancel_bet', bet_id)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith('GAS transfer was not successful'))
+
+    def test_get_bet_success_on_going(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+        result = self.engine.run(self.nef_path, 'get_bet', bet_id)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+        self.assertIsInstance(result, list)
+        self.assertEqual(5, len(result))
+
+        if isinstance(result[1], str):      # test engine converts to string whenever is possible
+            result[1] = result[1].encode('utf-8')
+
+        self.assertEqual(bet_id, result[0])             # bet_id
+        self.assertEqual(creator_account, result[1])    # creator
+        self.assertEqual(description, result[2])        # description
+        self.assertEqual(options, result[3])            # options
+        self.assertIsNone(result[4])                    # result - is None because it's on going
+
+    def test_get_bet_success_finished(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+        winners = ['choice1']
+        self._finish_bet(creator_account, bet_id, winners)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+        result = self.engine.run(self.nef_path, 'get_bet', bet_id)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+        self.assertIsInstance(result, list)
+        self.assertEqual(5, len(result))
+
+        if isinstance(result[1], str):      # test engine converts to string whenever is possible
+            result[1] = result[1].encode('utf-8')
+
+        self.assertEqual(bet_id, result[0])             # bet_id
+        self.assertEqual(creator_account, result[1])    # creator
+        self.assertEqual(description, result[2])        # description
+        self.assertEqual(options, result[3])            # options
+        self.assertEqual(winners, result[4])            # result
+
+    def test_get_bet_success_cancelled(self):
+        self.engine.reset_engine()
+
+        creator_account = bytes(20)
+        description = 'Bet for testing'
+        options = ['choice1', 'choice2', 'choice3']
+
+        bet_id = self._create_bet(creator_account, description, options)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+        self._cancel_bet(creator_account, bet_id)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+
+        result = self.engine.run(self.nef_path, 'get_bet', bet_id)
+        self.assertEqual(VMState.HALT, self.engine.vm_state)
+        self.assertIsInstance(result, list)
+        self.assertEqual(5, len(result))
+
+        if isinstance(result[1], str):      # test engine converts to string whenever is possible
+            result[1] = result[1].encode('utf-8')
+
+        self.assertEqual(bet_id, result[0])                 # bet_id
+        self.assertEqual(creator_account, result[1])        # creator
+        self.assertEqual(description, result[2])            # description
+        self.assertEqual(options, result[3])                # options
+        self.assertEqual('Cancelled by owner', result[4])   # result
+
+    def test_get_bet_fail_doesnt_exist(self):
+        self.engine.reset_engine()
+        bet_id = bytes(32)
+
+        self.engine.run(self.nef_path, 'get_bet', bet_id)
+        self.assertEqual(VMState.FAULT, self.engine.vm_state)
+        self.assertTrue(self.engine.error.endswith("Bet doesn't exist."))
+
